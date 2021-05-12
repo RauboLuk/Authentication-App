97a9c3d4f7ad8b0718f29228c44f7482e78a0b6a
RauboLuk <rauboluk@gmail.com>
2021-05-12 18:07:48 +0200

    services - login, logout, signup, getProfile, update

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,11 +3,21 @@ axios.defaults.withCredentials = true;
 
 const API_URL = "http://localhost:3000/api/auth/";
 
-const signout = () => {
+const login = (data) => {
+  return axios.post(API_URL + "login", data);
+};
+
+const logout = () => {
   return axios.get(API_URL + "logout");
+};
+
+const signup = (data) => {
+  return axios.post(API_URL + "signup", data);
 };
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
-  signout,
+  login,
+  logout,
+  signup,
 };
--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,7 +3,7 @@ axios.defaults.withCredentials = true;
 
 const API_URL = "http://localhost:3000/api/user/";
 
-const profile = () => {
+const getProfile = () => {
   return axios.get(API_URL + "profile");
 };
 
@@ -11,13 +11,8 @@ const update = (data) => {
   return axios.put(API_URL + "profile", data);
 };
 
-const signout = () => {
-  return axios.get(API_URL + "logout");
-};
-
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
-  profile,
+  getProfile,
   update,
-  signout,
 };
