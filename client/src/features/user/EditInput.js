504c44d2eabb6fc14091be507fa0e6dc8861e419
RauboLuk <rauboluk@gmail.com>
2021-05-14 22:43:49 +0200

    red border when edit form input validation failed

--- client/src/features/user/EditInput.js
+++ client/src/features/user/EditInput.js
@@ -1,24 +1,19 @@
 import "./EditInput.css";
 
-const EditInput = ({
-  label,
-  register,
-  area,
-  error,
-  ...props
-}) => {
+const EditInput = ({ label, register, area, error, ...props }) => {
   return (
     <section>
       <label className="input__label">{label}</label>
       {area ? (
         <textarea
-          className="input__textarea"
+          className={"input__textarea" + (error ? " input__input--br" : "")}
           rows="3"
           {...register(label)}
+          {...props}
         ></textarea>
       ) : (
         <input
-          className="input__input"
+          className={"input__input" + (error ? " input__input--br" : "")}
           {...register(label)}
           {...props}
         />
