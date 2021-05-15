45942e97b1609c1e02cbf3d264eb93ac2b527411
RauboLuk <rauboluk@gmail.com>
2021-05-15 16:45:08 +0200

    loading bar when user is logged in with session storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import "./App.css";
 
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUser, selectUser } from "./features/user/userSlice";
+import {
+  fetchUser,
+  selectUser,
+  selectUserStatus,
+} from "./features/user/userSlice";
 import ErrorSnackbar from "./features/user/ErrorSnackbar";
 import AuthContainer from "./features/user/AuthContainer";
 import Login from "./features/user/Login";
@@ -20,11 +25,16 @@ import Footer from "./app/Footer";
 
 function App() {
   const user = useSelector(selectUser);
+  const status = useSelector(selectUserStatus);
+  const isLoggedIn = sessionStorage.getItem("isLoggedIn");
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchUser());
   }, [dispatch]);
+
+  if (isLoggedIn && !user && (status === "loading" || status === "idle"))
+    return <LinearProgress />;
 
   if (!user) {
     return (
@@ -50,6 +60,7 @@ function App() {
 
   return (
     <Router>
+      <ErrorSnackbar />
       <Header
         name={user.name || user.email?.split("@")[0] || "undefined"}
         avatarUrl={user.img}
