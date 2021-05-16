3590cd005cee79f768c237fb046e7ba0126bbe92
RauboLuk <rauboluk@gmail.com>
2021-05-16 17:38:35 +0200

    incorrect email, password when login - test

--- server/tests/auth_api.test.js
+++ server/tests/auth_api.test.js
@@ -1,19 +1,60 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const User = require("../models/user");
 
 request = supertest(app);
 
-test("user is logged in", async () => {
-  await request
-    .post("/api/auth/login")
-    .send({ email: "john@wp.pl", password: "dolores" })
-    .set("Accept", "application/json")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("auth api", () => {
+  const user = { email: "john1@wp.pl", password: "dolores" };
+
+  test("signup user", async () => {
+    const response = await request
+      .post("/api/auth/signup")
+      .send(user)
+      .set("Accept", "application/json")
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toMatchObject({ email: user.email });
+  });
+
+  test("409 error with message when signup with email that already exist", async () => {
+    const response = await request
+      .post("/api/auth/signup")
+      .send(user)
+      .set("Accept", "application/json")
+      .expect(409)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toMatchObject({ error: "email is already taken" });
+  });
+
+  test("user can login", async () => {
+    const response = await request
+      .post("/api/auth/login")
+      .send(user)
+      .set("Accept", "application/json")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toMatchObject({ email: user.email });
+  });
+
+  test("login, 404 error if password or email is incorrect", async () => {
+    const response = await request
+      .post("/api/auth/login")
+      .send({ email: "jo@wp.pl", password: "dolores" })
+      .set("Accept", "application/json")
+      .expect(404)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body).toMatchObject({ error: "incorrect email or password" });
+  });
 });
 
 afterAll(async (done) => {
+  await User.deleteMany({});
   await mongoose.connection.close();
   done();
 });
--- server/utils/config.js
+++ server/utils/config.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CLIENT_ID = process.env.CLIENT_ID;
-const MONGODB_URI = process.env.NODE_ENV === 'test'   ? process.env.TEST_MONGODB_URI  : process.env.MONGODB_URI;
-// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
 
 module.exports = {
